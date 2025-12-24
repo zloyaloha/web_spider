c8eae6fb2d11e5ea49113915402bd90244405d30
Vladimir Filippov <zloyaloha@gmail.com>
2025-12-24 14:49:58 +0300

    add template method for indexator
    Signed-off-by: Vladimir Filippov <zloyaloha@gmail.com>

--- searcher/include/indexator.h
+++ searcher/include/indexator.h
@@ -10,17 +10,18 @@ protected:
 
 public:
     IIndexator(std::shared_ptr<RamIndexSource> src, std::shared_ptr<Tokenizer> tok);
-    virtual void addDocument(const std::string_view& url_view, const std::string_view& doc_view) = 0;
+    virtual void addDocument(const std::string_view& url_view, const std::string_view& doc_view);
+    virtual void processTokens(const std::vector<std ::string>& tokens, int doc_id) = 0;
 };
 
 class BooleanIndexator : public IIndexator {
 public:
     BooleanIndexator(std::shared_ptr<RamIndexSource> src, std::shared_ptr<Tokenizer> tok);
-    void addDocument(const std::string_view& url_view, const std::string_view& doc_view) override;
+    void processTokens(const std::vector<std ::string>& tokens, int doc_id) override;
 };
 
 class TFIDFIndexator : public IIndexator {
 public:
     TFIDFIndexator(std::shared_ptr<RamIndexSource> src, std::shared_ptr<Tokenizer> tok);
-    void addDocument(const std::string_view& url_view, const std::string_view& doc_view) override;
+    void processTokens(const std::vector<std ::string>& tokens, int doc_id) override;
 };
